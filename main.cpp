7acb23592b956fd57a026fd5120d3f8d14280107
icicala <icicala.ubb@gmail.com>
2022-07-01 17:36:24 +0200

    feat(Exercise 1) Exercise 1.4 integrating a function with the rectangle method

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cmath>
+double f(double x){
+    return 1/exp(pow(x, 2.0));
+}
+
+double integral(double a, double b, int n){
+    double interval = std::abs(b - a) / n;
+    double sum{};
+    for(int i = n; i >=0; --i){
+        sum+=interval*f(a);
+        a+=interval;
+    }
+    return sum;
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Test" << std::endl;
+    std::cout << integral(0,1, 10000000);
     return 0;
 }
