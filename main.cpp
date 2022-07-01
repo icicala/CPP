02075e3c19a6c105b6f4a22ac402e3d0338248cf
icicala <icicala.ubb@gmail.com>
2022-07-01 17:44:29 +0200

    feat(Exercise 1) Exercise 1.5 macro SQUARESUM

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#define SQUARESUM(a, b) (((a)*(a))+((b)*(b)))
+using std::cout, std::endl;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int i1 = 3;
+    int i2 = 2; // Ergebnis:
+    cout << SQUARESUM(i1, i2) << endl; // 13
+    cout << SQUARESUM(2 + i1, i2) << endl; // 29
+    cout << SQUARESUM(i1, i2 + 1) << endl; // 18
+    cout << 3 * SQUARESUM(i1, i2) << endl; // 39
     return 0;
 }
