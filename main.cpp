32f51189e8d2453aac0cfc3e8d470a328fa236b1
icicala <icicala.ubb@gmail.com>
2022-07-02 18:15:58 +0200

    feat(Exercise 2) Exercise 2.3 Theoretical exercise

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int v[] = {10, 20, 30, 40};
+    int i, *pv;
+    /**
+     * a) 10 20 30 40
+     */
+//    for (pv = v; pv <= v + 3; pv++)
+//        std::cout << *pv << std::endl;
+
+    /**
+     * b)20 30 40
+     */
+//    for (pv = v, i = 1; i <= 3; i++)
+//        std::cout << pv[i] << std::endl;
+
+    /**
+     * c) 10 30
+     */
+    for (pv = v, i = 0; pv+i <= &v[3]; pv++, i++)
+        std::cout << *(pv + i) << std::endl;
+
     return 0;
 }
