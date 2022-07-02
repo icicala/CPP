212641ec60781ed6155a114f0c3aca1e73bb675d
icicala <icicala.ubb@gmail.com>
2022-07-02 17:50:49 +0200

    feat(Exercise 2) Exercise 2.2 Theoretical Exercise

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int i1 = 1;
+int i2 = 2;
+void func(int & ref1, int & ref2) {
+    int * ptr1;
+    int * ptr2;
+    ptr1 = &i1;
+    ref2 = *ptr1;
+    ptr2 = ptr1;
+    *ptr2 = 4;
+    ref1 = 5;
+    ref2 = *ptr2;
+}
+int main(int argc, char* argv[])
+{
+    func(i1, i2);
+    std::cout << "i1 = " << i1 << std::endl;
+    std::cout << "i2 = " << i2 << std::endl;
     return 0;
+
+    /**
+     * i1 = 5
+       i2 = 5
+     */
 }
