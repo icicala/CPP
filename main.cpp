41bf3f673f1a6951d44d7866320f5dcae0fe353d
icicala <icicala.ubb@gmail.com>
2022-07-02 17:43:42 +0200

    feat(Exercise 2) Exercise 2.1 Calculate Surface and Volume of Sphere

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <cmath>
+void calcSurfaceAndVolumeOfSphere(const double& radius, double& surface, double& volume){
+
+    surface = 4*M_PI*pow(radius,2);
+    volume = (4.0/3) * M_PI * pow(radius, 3);
+
+}
+void calcSurfaceAndVolumeOfSphere(const double* radius, double* surface, double* volume){
+
+    *surface = 4*M_PI*pow(*radius,2.0);
+    *volume = (4.0/3) * M_PI * pow(*radius, 3);
+
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    double surface{};
+    double volume{};
+    double radius{0.5};
+    for(int i = 0; i<=20; ++i){
+        //calcSurfaceAndVolumeOfSphere(radius, surface, volume);
+        calcSurfaceAndVolumeOfSphere(&radius, &surface, &volume);
+
+
+        std::cout << "Radius:" << radius <<" Surface:" << surface << " Volume:" << volume << std::endl;
+        radius+=0.5;
+    }
+
     return 0;
 }
