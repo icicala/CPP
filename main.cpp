1df28c9bf04aeb39c457e19f73b20dbc06cf77df
icicala <icicala.ubb@gmail.com>
2022-07-03 14:57:05 +0200

    feat(Exercise 3) Exercise 1.1 Theoretical exercise

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+using namespace std;
+class KL1 {
+private:
+    string name;
+public:
+    int i;
+    KL1(){
+        name = "### not defined ###";
+        i=0;
+    }
+    KL1(string n, int v) : name(n), i(v){}
+    void print() {
+        cout << name << ": " << i << endl;
+    }
+};
+class KL2 {
+private:
+    void meth2(KL1& ok, int wert) {
+        ok.i = wert;
+        wert = 888;
+    }
+public:
+    void test() {
+        KL1 k0("obj1", 10);
+        KL1 k1("obj2", 20);
+        KL1& k2 = k1;
+        KL1 feld[3];
+        feld[0] = k0;
+        feld[1] = k1;
+        feld[2] = k2;
+        feld[0].print();
+        feld[1].print();
+        feld[2].print();
+        int j = 42;
+        meth2(feld[0], j);
+        feld[2].i = 999;
+        feld[0].print();
+        feld[1].print();
+        feld[2].print();
+        cout << "j = " << j << endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    KL2 o2;
+    o2.test();
     return 0;
 }
