122544d46fc2fb8c0ebb8888b3674829a7a122b0
icicala <icicala.ubb@gmail.com>
2022-07-12 23:04:37 +0200

    feat(Exercise 9) Exercise 9.1 Reverse Polish Notation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,67 @@
+
+
+#include <stack>
+#include <string>
+#include <cmath>
 #include <iostream>
 
+/**
+ * Exercise 9.1 Calculator Reverse Polish Notation
+ * @return
+ */
+bool isOperator(char ch){
+    if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+        return true;
+    }
+    return false;
+}
+
+bool isOperand(char ch){
+    if(ch >= '0' && ch <= '9'){
+        return true;
+    }
+    return false;
+}
+
+double operation(double a, double b, char op){
+    switch (op) {
+        case '+':
+            return b+a;
+        case '-':
+            return b-a;
+        case '*':
+            return b*a;
+        case '/':
+            return b/a;
+        default:
+            return NAN;
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+   std::string rpn { "55*22*+"};
+   double a;
+   double b;
+   std::stack<double> stack;
+   std::string::iterator i;
+   for(i = rpn.begin(); i != rpn.end(); ++i){
+       if(isOperator(*i)){
+           a = stack.top();
+           stack.pop();
+           b = stack.top();
+           stack.pop();
+           stack.push(operation(a,b, *i));
+       }else if(isOperand(*i)){
+           double temp = (*i) - '0';
+           stack.push(temp);
+       }
+   }
+   double result = stack.top();
+   stack.pop();
+   std::cout << result;
+
+
+
     return 0;
 }
