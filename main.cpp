7a906214d7f8a0dba745c4408c8e9d8844cf4b19
icicala <icicala.ubb@gmail.com>
2022-07-13 17:26:46 +0200

    feat(Exercise 9) Exercise 9.4 Count occurence of words in the text

--- main.cpp
+++ main.cpp
@@ -1,66 +1,34 @@
 
 
-#include <stack>
+
 #include <string>
-#include <cmath>
+#include <regex>
 #include <iostream>
 
-/**
- * Exercise 9.1 Calculator Reverse Polish Notation
- * @return
- */
-bool isOperator(char ch){
-    if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
-        return true;
-    }
-    return false;
-}
-
-bool isOperand(char ch){
-    if(ch >= '0' && ch <= '9'){
-        return true;
-    }
-    return false;
-}
-
-double operation(double a, double b, char op){
-    switch (op) {
-        case '+':
-            return b+a;
-        case '-':
-            return b-a;
-        case '*':
-            return b*a;
-        case '/':
-            return b/a;
-        default:
-            return NAN;
-    }
-}
-
 int main() {
+    std::string value = "Develop a program, which is able to count the occurrence of words in a text. As a result, the program prints out each word found in the text together with the number of occurrences of this specific word in the text. As an example, you see the output of the program, if you provide the text of this exercise as input";
 
-   std::string rpn { "55*22*+"};
-   double a;
-   double b;
-   std::stack<double> stack;
-   std::string::iterator i;
-   for(i = rpn.begin(); i != rpn.end(); ++i){
-       if(isOperator(*i)){
-           a = stack.top();
-           stack.pop();
-           b = stack.top();
-           stack.pop();
-           stack.push(operation(a,b, *i));
-       }else if(isOperand(*i)){
-           double temp = (*i) - '0';
-           stack.push(temp);
-       }
-   }
-   double result = stack.top();
-   stack.pop();
-   std::cout << result;
 
+
+  std::string::iterator v = value.begin();
+  std::string output (value.length(), 'X');
+    std::transform(value.begin(), value.end(), output.begin() , ::tolower);
+    std::regex re("[' ',.]");
+    std::sregex_token_iterator first{output.begin(), output.end(), re, -1}, last;//the '-1' is what makes the regex split (-1 := what was not matched)
+    std::vector<std::string> tokens{first, last};
+    std::map<std::string, int> result;
+    for(auto& t:tokens){
+        if(!t.empty()) {
+            std::pair<std::map<std::string, int>::iterator, bool> ptr = result.insert(std::make_pair(t, 1));
+            if(!ptr.second){
+                ptr.first->second +=1;
+            }
+        }
+
+    }
+    for(auto & it : result){
+        std::cout << it.first << ": " << it.second << std::endl;
+    }
 
 
     return 0;
